15e8e4aeecf95df77a2c8035f02234ccde11bff6
AK391 <ahsengradio@gmail.com>
2021-04-03 01:19:08 -0400

    delete extra p

--- demo.py
+++ demo.py
@@ -18,7 +18,7 @@ outputs =  gr.outputs.Textbox(label="Output Text")
 
 title = "wav2vec 2.0"
 description = "demo for Facebook AI wav2vec 2.0 using Hugging Face transformers. To use it, simply upload your audio, or click one of the examples to load them. Read more at the links below."
-article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2006.11477'>wav2vec 2.0: A Framework for Self-Supervised Learning of Speech Representations</a> | <a href='https://github.com/pytorch/fairseq'>Github Repo</a></p> | <a href='https://huggingface.co/facebook/wav2vec2-base-960h'>Hugging Face model</a></p>"
+article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2006.11477'>wav2vec 2.0: A Framework for Self-Supervised Learning of Speech Representations</a> | <a href='https://github.com/pytorch/fairseq'>Github Repo</a> | <a href='https://huggingface.co/facebook/wav2vec2-base-960h'>Hugging Face model</a></p>"
 examples = [
     ["poem.wav"]
 ]
